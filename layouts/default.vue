23c7d3b0a9e59eace29a8ff97e1d28e092c3dd81
dark-menacing <evertondoli.candido@gmail.com>
2025-01-01 20:04:34 -0300

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header>
+      <NuxtLink to="/">
+        Home
+      </NuxtLink>
+    </header>
+    <br />
+    <slot />
+    <br />
+    <footer>
+      <NuxtLink to="https://github.com/dark-menacing/nuxt-website">
+        <NuxtImg src="/svg/github.svg" width="40px" height='40px' alt="Source code"/>
+      </NuxtLink>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import '/assets/scss/index.scss';
+</script>
