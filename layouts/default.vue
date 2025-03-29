4b740c2a83233c60796c65132eea730ccbc2b8e9
dark-menacing <evertondoli.candido@gmail.com>
2025-03-29 13:44:40 -0300

    Added main files

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <header>
+      <a href="/">
+        <Icon name="ri:sun-fill" style="color:black; font-size: 6em;"/>
+      </a>
+    </header>
+    <slot />
+    <footer>
+      <a href="github.com/dark-menacing/nuxt-website">
+        <Icon name="ri:github-fill" style="color: black; font-size: 3em;"/>
+      </a>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import '~/assets/css/index.css';
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, soluta.</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
